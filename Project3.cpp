80312d3f4e5eb08495fd4c02a42b4f6c4d097084
ceeddings <79119369+ceeddings@users.noreply.github.com>
2021-03-19 18:24:13 -0400

    Project3
    Make

---
+++ Project3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string sense; // One day this will all make ____
+	cout << "make";
+	cin >> sense;
+	/* Now print  One day this will all make , and sense */
+	cout << "One day this will all make" << sense;
+	return 0;
+}
